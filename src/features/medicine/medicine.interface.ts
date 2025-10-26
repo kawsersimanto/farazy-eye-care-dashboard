6d004b0847860c7fc87e983dc34c49d062a5ec73
Kawser Simanto <kawsersimanto@gmail.com>
2025-10-27 02:21:42 +0600

    created medicine interface

--- src/features/medicine/medicine.interface.ts
+++ src/features/medicine/medicine.interface.ts
@@ -1,3 +1,25 @@
+import { IMedicineBrand } from "@/features/medicine-brand/medicine-brand.interface";
+import { IMedicineCategory } from "@/features/medicine-category/medicine-category.interface";
+
 export interface IMedicine {
-  id: string;
+  id?: string;
+  name: string;
+  genericName: string;
+  strength: string | null;
+  unit: string | null;
+  type: string | null;
+  description: string | null;
+  sideEffects: string | null;
+  dosageForm: string | null;
+  routeOfAdmin: string | null;
+  unitPrice: number | null;
+  mrp: number | null;
+  isActive: boolean;
+  isPrescriptionOnly: boolean | null;
+  categoryId: string;
+  brandId: string;
+  brand?: IMedicineBrand;
+  category?: IMedicineCategory;
+  createdAt?: string;
+  updatedAt?: string;
 }
