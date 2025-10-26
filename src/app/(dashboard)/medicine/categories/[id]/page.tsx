97e7de5e7ff73e625316813d678499a18a404bb7
Kawser Simanto <kawsersimanto@gmail.com>
2025-10-27 02:12:01 +0600

    medicine category view

--- src/app/(dashboard)/medicine/categories/[id]/page.tsx
+++ src/app/(dashboard)/medicine/categories/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { MedicineCategory } from "@/features/medicine-category/components/MedicineCategory";
 
 const CategoryDetailsPage = async ({
   params,
@@ -7,17 +8,15 @@ const CategoryDetailsPage = async ({
 }) => {
   const { id } = await params;
 
-  console.log(id);
-
   return (
     <div>
       <Card className="max-w-4xl mx-auto px-10">
         <CardTitle className="flex items-center gap-3">
-          <h2 className="font-work-sans font-medium text-lg">
-            Create Category
-          </h2>
+          <h2 className="font-work-sans font-medium text-lg">View Category</h2>
         </CardTitle>
-        <CardDescription>Details</CardDescription>
+        <CardDescription>
+          <MedicineCategory id={id} />
+        </CardDescription>
       </Card>
     </div>
   );
--- src/features/medicine-category/components/MedicineCategory.tsx
+++ src/features/medicine-category/components/MedicineCategory.tsx
@@ -1,3 +1,52 @@
-export const MedicineCategory = () => {
-  return <div>MedicineCategory</div>;
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { useGetMedicineCategoryByIdQuery } from "../medicine-category.api";
+import { MedicineCategoryFormSkeleton } from "./MedicineCategoryFormSkeleton";
+
+export const MedicineCategory = ({ id }: { id: string }) => {
+  const { data, isLoading } = useGetMedicineCategoryByIdQuery(id);
+  const medicineCategory = data?.data;
+
+  if (isLoading) return <MedicineCategoryFormSkeleton />;
+
+  return (
+    <div>
+      <div>
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-xl font-semibold">
+            {medicineCategory?.name}
+          </CardTitle>
+          <Badge variant={medicineCategory?.isActive ? "default" : "secondary"}>
+            {medicineCategory?.isActive ? "Active" : "Inactive"}
+          </Badge>
+        </div>
+
+        <p className="text-sm text-muted-foreground mt-1">
+          Description: {medicineCategory?.description || "Not set"}
+        </p>
+      </div>
+
+      <Separator className="mt-5" />
+
+      <div className="pt-4 space-y-0">
+        <div className="flex items-center justify-between pt-2 text-sm text-muted-foreground">
+          <p>
+            Created:{" "}
+            {new Date(
+              medicineCategory?.createdAt as string
+            ).toLocaleDateString()}
+          </p>
+          <p>
+            Last Updated:{" "}
+            {new Date(
+              medicineCategory?.updatedAt as string
+            ).toLocaleDateString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
