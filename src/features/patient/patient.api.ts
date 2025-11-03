ed9195a1c6452cc3524ba55d84098faeb479cefc
Kawser Simanto <kawsersimanto@gmail.com>
2025-11-04 00:48:11 +0600

    added get api for patient

--- src/features/patient/patient.api.ts
+++ src/features/patient/patient.api.ts
@@ -1,11 +1,18 @@
 import { baseApi } from "@/redux/api/baseApi";
 import { ApiResponse } from "@/types/api";
+import { IRole } from "../user/user.interface";
 import { IPatient } from "./patient.interface";
 
 export const patientApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getPatients: builder.query<ApiResponse<IPatient[]>, void>({
-      query: () => "/patient",
+    getPatients: builder.query<
+      ApiResponse<IPatient[]>,
+      { branchId: string; role?: string }
+    >({
+      query: ({ branchId, role = IRole.PATIENT }) => ({
+        url: "/users",
+        params: { branchId, role },
+      }),
       providesTags: ["patient"],
     }),
     getPatientById: builder.query<ApiResponse<IPatient>, string>({
@@ -16,16 +23,17 @@ export const patientApi = baseApi.injectEndpoints({
       query: (body) => ({ url: "/patient", method: "POST", body }),
       invalidatesTags: ["patient"],
     }),
-    updatePatient: builder.mutation<IPatient, Partial<IPatient> & { id: string }>(
-      {
-        query: ({ id, ...body }) => ({
-          url: `/patient/${id}`,
-          method: "PUT",
-          body,
-        }),
-        invalidatesTags: ["patient"],
-      }
-    ),
+    updatePatient: builder.mutation<
+      IPatient,
+      Partial<IPatient> & { id: string }
+    >({
+      query: ({ id, ...body }) => ({
+        url: `/patient/${id}`,
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: ["patient"],
+    }),
     deletePatient: builder.mutation<{ success: boolean; id: string }, string>({
       query: (id) => ({ url: `/patient/${id}`, method: "DELETE" }),
       invalidatesTags: ["patient"],
