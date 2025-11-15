f2ec85473cafca047acf8bb8f95844188cb449ca
Kawser Simanto <kawsersimanto@gmail.com>
2025-11-16 02:58:16 +0600

    schedule interface added

--- src/features/schedule/schedule.interface.ts
+++ src/features/schedule/schedule.interface.ts
@@ -1,3 +1,12 @@
 export interface ISchedule {
   id: string;
+  doctorId: string;
+  dayOfWeek: string;
+  startTime: string;
+  endTime: string;
+  slotMinutes: number;
+  maxPatients: number;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
