a121312250c31762349e57ac14113ee767e9e775
kawserimanto <kawsersimanto@gmail.com>
2025-11-16 15:46:04 +0600

    fixed middleware for accessing public folder

--- src/middleware.ts
+++ src/middleware.ts
@@ -74,5 +74,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:jpg|jpeg|png|gif|svg|webp|ico)).*)",
+  ],
 };
