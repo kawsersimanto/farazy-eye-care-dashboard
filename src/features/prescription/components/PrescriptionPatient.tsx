f60740fb927ee6e0f6198efea6d43fe3017aa2ff
Kawser Simanto <kawsersimanto@gmail.com>
2025-11-20 01:37:05 +0600

    separated the prescription patient portion

--- src/features/prescription/components/PrescriptionPatient.tsx
+++ src/features/prescription/components/PrescriptionPatient.tsx
@@ -1,32 +1,114 @@
 "use client";
 
-import { useGetUserByIdQuery } from "@/features/user/user.api";
-import { formatTodayDate, getAgeFromISO } from "@/utils/date";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useGetPatientsQuery } from "@/features/patient/patient.api";
+import { IUser } from "@/features/user/user.interface";
+import { useDebounce } from "@/hooks/useDebounce";
+import { cn } from "@/lib/utils";
+import { useAppSelector } from "@/redux/hook";
+import { getAgeFromISO } from "@/utils/date";
+import { Check, Search } from "lucide-react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setSelectedPatient } from "../store/prescription.slice";
 
 export const PrescriptionPatient = () => {
-  const { data: patientData } = useGetUserByIdQuery("6917665d84030b43c9ed6a36");
-  const patient = patientData?.data;
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [page, setPage] = useState(1);
+  const [searchInput, setSearchInput] = useState("");
+  const debouncedSearch = useDebounce(searchInput, 500);
+  const selectedPatient = useAppSelector((state) => state.prescription.patient);
+  const { data } = useGetPatientsQuery({
+    limit: 50,
+    page,
+    searchTerm: debouncedSearch,
+  });
+
+  const users = data?.data?.data;
+
+  const handleSearch = (query: string) => {
+    setSearchInput(query);
+    setPage(1);
+  };
+
+  const handleSelectPatient = (user: IUser) => {
+    const age = user.patientProfile?.dateOfBirth
+      ? getAgeFromISO(user.patientProfile.dateOfBirth)
+      : 18;
+
+    dispatch(
+      setSelectedPatient({
+        id: user.id,
+        name: user.name || "",
+        phone: user.phone || "",
+        age,
+        gender: user.patientProfile?.gender || "MALE",
+      })
+    );
+    setOpen(false);
+    setSearchInput("");
+  };
 
   return (
-    <div className="flex justify-between items-center gap-4">
-      <div className="text-base">
-        <span className="font-medium text-foreground">Name:</span>{" "}
-        {patient?.name}
-      </div>
-      <div className="text-base">
-        <span className="font-medium text-foreground">Sex:</span>{" "}
-        {patient?.patientProfile?.gender}
-      </div>
-      <div className="text-base">
-        <span className="font-medium text-foreground">Age: </span>
-        {patient?.patientProfile?.dateOfBirth
-          ? getAgeFromISO(patient?.patientProfile?.dateOfBirth) + " Years"
-          : "-"}
-      </div>
-      <div className="text-base">
-        <span className="font-medium text-foreground">Date:</span>{" "}
-        {formatTodayDate()}
-      </div>
-    </div>
+    <>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            className={cn("w-full justify-between bg-transparent")}
+          >
+            {selectedPatient?.name || "Select Patient"}
+            <Search className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            {/* <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" /> */}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full p-0">
+          <Command shouldFilter={false}>
+            <CommandInput
+              placeholder="Search patient..."
+              value={searchInput}
+              onValueChange={handleSearch}
+            />
+            <CommandList>
+              <CommandEmpty>No patient found.</CommandEmpty>
+              <CommandGroup>
+                {users?.map((user) => (
+                  <CommandItem
+                    value={user.id}
+                    key={user.id}
+                    onSelect={() => handleSelectPatient(user)}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        selectedPatient?.id === user.id
+                          ? "opacity-100"
+                          : "opacity-0"
+                      )}
+                    />
+                    {user.name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 };
