2df89cf5d216d1e7b33b846ca5a1e07249695f39
Kawser Simanto <kawsersimanto@gmail.com>
2025-11-20 07:49:08 +0600

    added fonts

--- src/features/prescription-pdf/components/PrescriptionPdf.tsx
+++ src/features/prescription-pdf/components/PrescriptionPdf.tsx
@@ -1,5 +1,57 @@
-import { Document, Page, Text, View } from "@react-pdf/renderer";
+import { Document, Font, Page, Text, View } from "@react-pdf/renderer";
 import { PrescriptionPdfStyle } from "./PrescriptionPdfStyle";
+
+Font.register({
+  family: "Space Grotesk",
+  fonts: [
+    {
+      src: "/fonts/space-grotesk/spacegrotesk-regular.ttf",
+      fontWeight: 400,
+      fontStyle: "normal",
+    },
+    {
+      src: "/fonts/space-grotesk/spacegrotesk-medium.ttf",
+      fontWeight: 500,
+      fontStyle: "normal",
+    },
+    {
+      src: "/fonts/space-grotesk/spacegrotesk-semibold.ttf",
+      fontWeight: 600,
+      fontStyle: "normal",
+    },
+    {
+      src: "/fonts/space-grotesk/spacegrotesk-bold.ttf",
+      fontWeight: 700,
+      fontStyle: "normal",
+    },
+  ],
+});
+
+Font.register({
+  family: "Work Sans",
+  fonts: [
+    {
+      src: "/fonts/work-sans/worksans-regular.ttf",
+      fontWeight: 400,
+      fontStyle: "normal",
+    },
+    {
+      src: "/fonts/work-sans/worksans-medium.ttf",
+      fontWeight: 500,
+      fontStyle: "normal",
+    },
+    {
+      src: "/fonts/work-sans/worksans-semibold.ttf",
+      fontWeight: 600,
+      fontStyle: "normal",
+    },
+    {
+      src: "/fonts/work-sans/worksans-bold.ttf",
+      fontWeight: 700,
+      fontStyle: "normal",
+    },
+  ],
+});
 
 export const PrescriptionPdf = () => (
   <Document>
--- src/features/prescription-pdf/components/PrescriptionPdfStyle.tsx
+++ src/features/prescription-pdf/components/PrescriptionPdfStyle.tsx
@@ -3,6 +3,8 @@ import { StyleSheet } from "@react-pdf/renderer";
 export const PrescriptionPdfStyle = StyleSheet.create({
   body: {
     backgroundColor: "#fff",
+    fontFamily: "Space Grotesk",
+    fontSize: 14,
   },
   section: {},
 });
